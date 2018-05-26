6e2f38ef67dc3970a976cfc8dbe9a403ffd1fc88
EliBildman <eli@Elis-MacBook-Pro.local>
2018-05-26 14:55:34 -0400

    inain

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 nums = [];
 means = [];
+min =
+max =
 
 function randRange(lower, upper) { // inclusive - uninclusive
   return Math.floor(Math.random() * (upper - lower)) + lower
@@ -13,6 +15,11 @@ function avergae(arr) {
     }
     return sum / arr.length;
   }
+}
+
+function start() {
+  min = document.getElementById("min").value;
+  min = document.getElementById("max").value;
 }
 
 function addN() {
