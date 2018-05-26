d416ca8d04fae96b20d811401aa979470ae7bb7a
EliBildman <eli@Elis-MacBook-Pro.local>
2018-05-26 14:49:37 -0400

    average

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+nums = [];
+means = [];
+
 function randRange(lower, upper) { // inclusive - uninclusive
   return Math.floor(Math.random() * (upper - lower)) + lower
 }
 
-for(let i = 0; i < 100; i++) {
-  console.log(randRange(1, 10));
+function avergae(arr) {
+  if(arr.length > 0) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum / arr.length;
+  }
 }
