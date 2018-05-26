cf9bcb77840e13d2a312ed6980877527f256deac
EliBildman <eli@Elis-MacBook-Pro.local>
2018-05-26 14:46:22 -0400

    fixed randRange

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function randRange(lower, upper) { // inclusive - uninclusive
-  return Math.floor(Math.random() * upper - lower) + lower
+  return Math.floor(Math.random() * (upper - lower)) + lower
+}
+
+for(let i = 0; i < 100; i++) {
+  console.log(randRange(1, 10));
 }
